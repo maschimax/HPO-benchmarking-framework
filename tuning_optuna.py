994352d1b4838207152843f538f02b0e11c2c9b6
maschimax <53035227+maschimax@users.noreply.github.com>
2020-09-15 15:39:17 +0200

    Added tuning of Keras-Regressor with optuna

--- tuning_optuna.py
+++ tuning_optuna.py
@@ -4,6 +4,8 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 from optuna.samplers import TPESampler
 from optuna.samplers import CmaEsSampler
+from optuna.samplers import RandomSampler
+from tensorflow import keras
 
 import preprocessing as pp
 
@@ -21,6 +23,7 @@ X_train, y_train, X_val, y_val, X_test = pp.process(train_raw, test_raw, standar
 
 
 # 1. Define an objective function to be maximized
+# Random Forest Regressor
 def rf_objective(trial):
     # 2. Suggest values for the hyperparameters using a trial object
     n_estimators = trial.suggest_int(name='n_estimators', low=1, high=200)
@@ -42,18 +45,67 @@ def rf_objective(trial):
     return val_loss
 
 
+# Keras Regressor
+def keras_objective(trial):
+
+    dropout_rate = trial.suggest_uniform(name='droput_rate', low=0.0, high=0.9)
+    lr = trial.suggest_uniform(name='lr', low=1e-6, high=1e-1)
+    width_1stlayer = trial.suggest_int(name='width_1stlayer', low=8, high=512, step=8)
+    num_hidden_layers = trial.suggest_int(name='num_hidden_layers', low=1, high=4)
+
+    model = keras.Sequential()
+
+    model.add(keras.layers.InputLayer(input_shape=len(X_train.keys())))
+
+    model.add(keras.layers.Dense(width_1stlayer, activation='relu'))
+
+    if num_hidden_layers > 0:
+        width_hidlayer1 = trial.suggest_int(name='width_hidlayer1', low=10, high=100, step=10)
+        model.add(keras.layers.Dense(width_hidlayer1, activation='relu'))
+
+    if num_hidden_layers > 1:
+        width_hidlayer2 = trial.suggest_int(name='width_hidlayer2', low=10, high=100, step=10)
+        model.add(keras.layers.Dense(width_hidlayer2, activation='relu'))
+
+    if num_hidden_layers > 2:
+        width_hidlayer3 = trial.suggest_int(name='width_hidlayer3', low=10, high=100, step=10)
+        model.add(keras.layers.Dense(width_hidlayer3, activation='relu'))
+
+    if num_hidden_layers > 3:
+        width_hidlayer4 = trial.suggest_int(name='width_hidlayer4', low=10, high=100, step=10)
+        model.add(keras.layers.Dense(width_hidlayer4, activation='relu'))
+
+    model.add(keras.layers.Dropout(dropout_rate))
+    model.add(keras.layers.Dense(1))
+
+    optimizer = keras.optimizers.RMSprop(learning_rate=lr)
+
+    model.compile(optimizer=optimizer, loss='mse')
+
+    model.fit(X_train, y_train, epochs=10, batch_size=128, validation_data=(X_val, y_val), verbose=1)
+    y_pred = model.predict(X_val)
+
+    val_loss = sqrt(mean_squared_error(y_val, y_pred))
+
+    return val_loss
+
+
 # 3. Create a study object an optimize the objective function
-SAMPLER = 'CMA-ES' # 'TPE', 'CMA-ES'
+SAMPLER = 'TPE'  # 'TPE', 'CMA-ES'
 
 if SAMPLER == 'TPE':
     thisSampler = TPESampler()
 elif SAMPLER == 'CMA-ES':
     thisSampler = CmaEsSampler()
+else:
+    thisSampler = RandomSampler()
 
-ALGORITHM = 'RandomForestRegressor' # 'RandomForestRegressor', 'Keras'
+ALGORITHM = 'Keras'  # 'RandomForestRegressor', 'Keras'
 
 if ALGORITHM == 'RandomForestRegressor':
     thisObjective = rf_objective
+elif ALGORITHM == 'Keras':
+    thisObjective = keras_objective
 
-study = optuna.create_study(sampler=thisSampler, direction='minimize',)
-study.optimize(func=rf_objective, n_trials=100)
+study = optuna.create_study(sampler=thisSampler, direction='minimize', )
+study.optimize(func=thisObjective, n_trials=100)
