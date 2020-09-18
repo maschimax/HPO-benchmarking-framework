527fd148fb2e0709ab17a9cfd41cf6300b784434
maschimax <53035227+maschimax@users.noreply.github.com>
2020-09-18 16:33:01 +0200

    Working state for the Trial class

--- hpo/results.py
+++ hpo/results.py
@@ -1,7 +1,7 @@
 class TuningResult():
-    def __init__(self, trial_ids: list, timestamps: list, losses: list, configurations: tuple, best_loss: float,
+    def __init__(self, evaluation_ids: list, timestamps: list, losses: list, configurations: tuple, best_loss: float,
                  best_configuration: dict):
-        self.trial_ids = trial_ids  # list
+        self.evaluation_ids = evaluation_ids  # list
         self.timestamps = timestamps  # list
         self.losses = losses  # list
         self.configurations = configurations  # tuple of dictionaries
