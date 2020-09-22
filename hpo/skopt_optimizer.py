98cbcdf7a24b828e3339054401c209b1a793510f
maschimax <53035227+maschimax@users.noreply.github.com>
2020-09-22 10:50:14 +0200

    Formatting and documentation

--- hpo/skopt_optimizer.py
+++ hpo/skopt_optimizer.py
@@ -16,6 +16,67 @@ class SkoptOptimizer(BaseOptimizer):
         super().__init__(hp_space, hpo_method, ml_algorithm, x_train, x_val, y_train, y_val, metric, budget,
                          random_seed)
 
+    def optimize(self) -> TuningResult:
+
+        # Select the corresponding objective function of the ML-Algorithm
+        if self.ml_algorithm == 'RandomForestRegressor' or self.ml_algorithm == 'SVR':
+            this_objective = self.objective_scikit_regressor
+
+        elif self.ml_algorithm == 'KerasRegressor':
+            this_objective = self.objective_keras_regressor
+
+        else:
+            raise NameError('Unknown ML-algorithm!')
+
+        # Select the specified HPO-tuning method
+        if self.hpo_method == 'SMAC':
+            this_optimizer = forest_minimize
+            this_acq_func = 'EI'
+
+        else:
+            raise NameError('Unknown HPO-method!')
+
+        # Optimize on the predefined budget and measure the wall clock times
+        start_time = time.time()
+        self.times = []  # Initialize a list for saving the wall clock times
+
+        # Start the optimization
+        trial_result = this_optimizer(this_objective, self.hp_space, n_calls=self.budget, random_state=self.random_seed,
+                                      acq_func=this_acq_func)
+
+        for i in range(len(self.times)):
+            # Subtract the start time to receive the wall clock time of each function evaluation
+            self.times[i] = self.times[i] - start_time
+        wall_clock_time = max(self.times)
+
+        # Create a TuningResult-object to store the optimization results
+        # Transformation of the results into a TuningResult-Object
+        best_loss = trial_result.fun
+        losses = list(trial_result.func_vals)  # Loss of each iteration
+
+        # Determine the best HP-configuration of this run
+        best_configuration = {}
+        for i in range(len(self.hp_space)):
+            best_configuration[self.hp_space[i].name] = trial_result.x[i]
+
+        # Number the evaluations / iterations of this run
+        evaluation_ids = list(range(1, len(trial_result.func_vals) + 1))
+
+        # Determine the HP-configuration of each evaluation / iteration
+        configurations = ()
+        for i in range(len(trial_result.x_iters)):
+            this_config = {}
+            for j in range(len(self.hp_space)):
+                this_config[self.hp_space[j].name] = trial_result.x_iters[i][j]
+            configurations = configurations + (this_config,)
+
+        # Pass the results to a TuningResult-object
+        result = TuningResult(evaluation_ids=evaluation_ids, timestamps=self.times, losses=losses,
+                              configurations=configurations, best_loss=best_loss, best_configuration=best_configuration,
+                              wall_clock_time=wall_clock_time)
+
+        return result
+
     def train_evaluate_scikit_regressor(self, params):
         """ This method trains a scikit-learn model according to the selected HP-configuration and returns the
         validation loss"""
@@ -28,6 +89,7 @@ class SkoptOptimizer(BaseOptimizer):
         else:
             raise NameError('Unknown ML-algorithm!')
 
+        # Train the model and make the prediction
         model.fit(self.x_train, self.y_train)
         y_pred = model.predict(self.x_val)
 
@@ -53,6 +115,7 @@ class SkoptOptimizer(BaseOptimizer):
         """ This method trains a Keras model according to the selected HP-configuration and returns the
         validation loss"""
 
+        # Initialize the neural network
         model = keras.Sequential()
 
         # Add input layer
@@ -86,14 +149,16 @@ class SkoptOptimizer(BaseOptimizer):
         else:
             raise NameError('Unknown learning rate schedule!')
 
+        # Determine the learning rate for this iteration and pass it as callback
         lr = keras.callbacks.LearningRateScheduler(schedule)
-
         callbacks_list = [lr]
 
+        # Train the model
         model.fit(self.x_train, self.y_train, epochs=self.budget, batch_size=params['batch_size'],
                   validation_data=(self.x_val, self.y_val), callbacks=callbacks_list,
                   verbose=1)
 
+        # Make the prediction
         y_pred = model.predict(self.x_val)
 
         # Compute the validation loss according to the metric selected
@@ -113,57 +178,3 @@ class SkoptOptimizer(BaseOptimizer):
             dict_params[self.hp_space[i].name] = params[i]
 
         return self.train_evaluate_keras_regressor(dict_params)
-
-    def optimize(self) -> TuningResult:
-
-        # Select the corresponding objective function of the ML-Algorithm
-        if self.ml_algorithm == 'RandomForestRegressor' or self.ml_algorithm == 'SVR':
-            this_objective = self.objective_scikit_regressor
-        elif self.ml_algorithm == 'KerasRegressor':
-            this_objective = self.objective_keras_regressor
-        else:
-            raise NameError('Unknown ML-algorithm!')
-
-        # Select the specified HPO-tuning method
-        if self.hpo_method == 'SMAC':
-            this_optimizer = forest_minimize
-            this_acq_func = 'EI'
-        else:
-            raise NameError('Unknown HPO-method!')
-
-        # Optimize on the predefined budget and measure the optimization time
-        start_time = time.time()
-        self.times = []  # Initialize a list for saving the optimization times
-
-        # Start the optimization
-        trial_result = this_optimizer(this_objective, self.hp_space, n_calls=self.budget, random_state=self.random_seed,
-                                      acq_func=this_acq_func)
-
-        # wall_clock_time = time.time() - start_time
-        for i in range(len(self.times)):
-            # Subtract to start time to receive the durations of each evaluation
-            self.times[i] = self.times[i] - start_time
-        wall_clock_time = max(self.times)
-
-        # Create a TuningResult-object to store the optimization results
-        # Transformation of the results into a TuningResult-Object
-        best_loss = trial_result.fun
-        losses = list(trial_result.func_vals)
-
-        best_configuration = {}
-        for i in range(len(self.hp_space)):
-            best_configuration[self.hp_space[i].name] = trial_result.x[i]
-
-        evaluation_ids = list(range(1, len(trial_result.func_vals) + 1))
-        configurations = ()
-        for i in range(len(trial_result.x_iters)):
-            this_config = {}
-            for j in range(len(self.hp_space)):
-                this_config[self.hp_space[j].name] = trial_result.x_iters[i][j]
-            configurations = configurations + (this_config,)
-
-        result = TuningResult(evaluation_ids=evaluation_ids, timestamps=self.times, losses=losses,
-                              configurations=configurations, best_loss=best_loss, best_configuration=best_configuration,
-                              wall_clock_time=wall_clock_time)
-
-        return result
