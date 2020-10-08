eb8b3c539f1b652112b532d618a2ff62a2676d68
Max <maximilian.motz@rwth-aachen.de>
2020-10-08 11:01:47 +0200

    Testing MongoDB for the parallelization of hyperopt

--- run.py
+++ run.py
@@ -32,12 +32,12 @@ if debug:
     # Set parameters manually
     hp_space = space_rf
     ml_algo = 'RandomForestRegressor'
-    opt_schedule = [('hpbandster', 'BOHB')]
+    opt_schedule = [('robo', 'Bohamiann')]
     # Possible schedule combinations [('optuna', 'CMA-ES'), ('optuna', 'RandomSearch'),
     # ('skopt', 'SMAC'), ('skopt', 'GPBO'), ('hpbandster', 'BOHB'), ('hpbandster', 'Hyperband'), ('robo', 'Fabolas'),
     # ('robo', 'Bohamiann'), ('hyperopt', 'TPE')]
     n_runs = 3
-    n_func_evals = 15
+    n_func_evals = 60
     n_workers = 4
     loss_metric = root_mean_squared_error
 
