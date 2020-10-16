a043077f7719e29e975382e119a8598bc239140a
maschimax <53035227+maschimax@users.noreply.github.com>
2020-10-16 23:46:58 +0200

    Trying to convert pd.DataFrame in tf.data.Dataset for parallelizing Keras training

--- hpo/outdated/parallelizing_keras.py
+++ hpo/outdated/parallelizing_keras.py
@@ -5,12 +5,12 @@ import preprocessing as pp
 from pathlib import Path
 from hpo.outdated.keras_sequence_testing import MySequence
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-os.environ.pop('TF_CONFIG', None)
-
-if '.' not in sys.path:
-    sys.path.insert(0, '.')
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+#
+# os.environ.pop('TF_CONFIG', None)
+#
+# if '.' not in sys.path:
+#     sys.path.insert(0, '.')
 
 import tensorflow as tf
 from tensorflow import keras
@@ -28,8 +28,8 @@ test_raw = pp.load_data(data_folder, test_file)
 X_train, y_train, X_val, y_val, X_test = pp.process(train_raw, test_raw, standardization=False, logarithmic=False,
                                                     count_encoding=False)
 
-train_seq = MySequence(X_train, y_train, batch_size=64)
-test_seq = MySequence(X_val, y_val, batch_size=64)
+# train_seq = MySequence(X_train, y_train, batch_size=64)
+# test_seq = MySequence(X_val, y_val, batch_size=64)
 
 
 def build_and_compile_keras_regressor(x_train):
@@ -50,6 +50,9 @@ def build_and_compile_keras_regressor(x_train):
 
 
 this_model = build_and_compile_keras_regressor(X_train)
-this_model.fit(x=train_seq, epochs=1000, batch_size=64, validation_data=(X_val, y_val), workers=4,
+
+dataset = tf.data.Dataset.from_tensor_slices((X_train.values, y_train.values))
+
+this_model.fit(x=dataset, epochs=1000, batch_size=64, validation_data=(X_val, y_val), workers=4,
                use_multiprocessing=True)
 bla = 0
