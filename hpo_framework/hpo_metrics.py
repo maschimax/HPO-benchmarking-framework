9aae8af66dcdf86771e7e531d882b8934c57e005
maschimax <maximilian.motz@rwth-aachen.de>
2020-10-24 13:13:59 +0200

    Working on LGBMClassifier

--- hpo_framework/hpo_metrics.py
+++ hpo_framework/hpo_metrics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import mean_squared_error, f1_score
+from sklearn.metrics import mean_squared_error, f1_score, accuracy_score
 from math import sqrt
 
 
@@ -7,7 +7,13 @@ def root_mean_squared_error(y_true, y_pred):
 
 
 def f1_loss(y_true, y_pred):
+    # >> according to the documentation, a weighted average makes sense for multiclass targets only!
     loss = 1 - f1_score(y_true=y_true, y_pred=y_pred, average='weighted')
+    return loss
+
+
+def accuracy_loss(y_true, y_pred):
+    loss = 1 - accuracy_score(y_true, y_pred)
     return loss
 
 
