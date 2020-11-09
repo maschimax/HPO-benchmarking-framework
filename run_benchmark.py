6e54bc69103c8d20c2dce200ab8252087ec40c0a
maschimax <maximilian.motz@rwth-aachen.de>
2020-11-09 17:47:04 +0100

    Steel data set: y_data in row vector format

--- run_benchmark.py
+++ run_benchmark.py
@@ -15,9 +15,10 @@ import datasets.dummy.preprocessing as pp
 from datasets.Scania_APS_Failure.scania_preprocessing import scania_loading_and_preprocessing
 from datasets.Turbofan_Engine_Degradation.turbofan_preprocessing import turbofan_loading_and_preprocessing
 from datasets.Mining_Process.mining_preprocessing import mining_loading_and_preprocessing
+from datasets.Faulty_Steel_Plates.steel_preprocessing import steel_loading_and_preprocessing
 
 # Flag for the ML use case / dataset to be used
-use_case = 'mining'
+use_case = 'steel'
 
 # Flag for debug mode (yes/no)
 # yes (True) -> set parameters for this trial in source code (below)
@@ -26,17 +27,17 @@ debug = True
 
 if debug:
     # Set parameters manually
-    hp_space = space_lgb
-    ml_algo = 'LGBMRegressor'
-    opt_schedule = [('hpbandster', 'BOHB')]
+    hp_space = space_rf_clf
+    ml_algo = 'RandomForestClassifier'
+    opt_schedule = [('optuna', 'TPE')]
     # Possible schedule combinations [('optuna', 'CMA-ES'), ('optuna', 'RandomSearch'),
     # ('skopt', 'SMAC'), ('skopt', 'GPBO'), ('hpbandster', 'BOHB'), ('hpbandster', 'Hyperband'), ('robo', 'Fabolas'),
     # ('robo', 'Bohamiann'), ('optuna', 'TPE')]
     n_runs = 2
     n_func_evals = 20
     n_workers = 1
-    loss_metric = root_mean_squared_error
-    loss_metric_str = 'root_mean_squared_error'
+    loss_metric = f1_loss
+    loss_metric_str = 'F1-loss'
     do_warmstart = 'No'
 
 else:
@@ -188,6 +189,10 @@ elif use_case == 'turbofan':
 elif use_case == 'mining':
 
     X_train, X_test, y_train, y_test = mining_loading_and_preprocessing()
+
+elif use_case == 'steel':
+
+    X_train, X_test, y_train, y_test = steel_loading_and_preprocessing()
 
 else:
     raise Exception('Unknown dataset / use-case.')
