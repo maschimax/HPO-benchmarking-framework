34c9261766740c287599117bf886116ac8bd0e71
maschimax <maximilian.motz@rwth-aachen.de>
2020-11-12 17:08:30 +0100

    Small bug fix for multiclass LightGBM classifier

--- hpo_framework/trial.py
+++ hpo_framework/trial.py
@@ -883,8 +883,9 @@ class Trial:
 
                     # Multiclass classification task
                     else:
-                        params['objective'] = 'multiclass'  # uses Softmax objective function
-                        params['num_class'] = num_classes
+                        params = {'objective': 'multiclass',  # uses Softmax objective function
+                                  'num_class': num_classes,
+                                  'seed': 0}
 
                 lgb_clf = lgb.train(params=params, train_set=train_data, valid_sets=[valid_data])
 
