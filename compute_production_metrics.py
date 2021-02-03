5099c2e3fa9e524ba43976e90c743b1db18b2d0f
maschimax <53035227+maschimax@users.noreply.github.com>
2021-02-03 17:24:37 +0100

    Script for computing different production-related ML metrics

--- compute_production_metrics.py
+++ compute_production_metrics.py
@@ -1,11 +1,14 @@
 import os
 import pandas as pd
+import numpy as np
 import ast
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.metrics import mean_absolute_error, confusion_matrix
+from sklearn.metrics import mean_absolute_error, confusion_matrix, accuracy_score, ConfusionMatrixDisplay
 from tensorflow import keras
-import pickle
+import json
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 from hpo_framework.baseoptimizer import BaseOptimizer
 from hpo_framework.trial import Trial
@@ -16,7 +19,7 @@ from datasets.Blisk.blisk_preprocessing import blisk_loading_and_preprocessing
 from datasets.Surface_Crack_Image.surface_crack_preprocessing import surface_crack_loading_and_preprocessing
 
 initial_path = './hpo_framework/results/'
-find_best_configs = False
+find_best_configs = True
 
 best_hp_configs = {
     'turbofan': {
@@ -62,10 +65,13 @@ best_hp_configs = {
 }
 
 new_metrics = {'turbofan': {'MAE': [None, None]},
-               'scania': {'Confusion Matrix': [None, None]},
-               'sensor': {'Confusion Matrix': [None, None]},
+               'scania': {'Confusion Matrix': [None, None],
+                          'Accuracy': [None, None]},
+               'sensor': {'Confusion Matrix': [None, None],
+                          'Accuracy': [None, None]},
                'blisk': {'MAE': [None, None]},
-               'surface': {'Confusion Matrix': [None, None]}}
+               'surface': {'Confusion Matrix': [None, None],
+                           'Accuracy': [None, None]}}
 
 max_rel_loss_improvement = 0.0
 best_ml_algo = None
@@ -157,26 +163,31 @@ for idx, row in config_df.iterrows():
 
         X_train, X_test, y_train, y_test = turbofan_loading_and_preprocessing()
         is_time_series = False
+        shuffle = True
 
     elif dataset == 'scania':
 
         X_train, X_test, y_train, y_test = scania_loading_and_preprocessing()
         is_time_series = False
+        shuffle = True
 
     elif dataset == 'sensor':
 
         X_train, X_test, y_train, y_test = sensor_loading_and_preprocessing()
         is_time_series = False
+        shuffle = True
 
     elif dataset == 'blisk':
 
         X_train, X_test, y_train, y_test = blisk_loading_and_preprocessing()
         is_time_series = True
+        shuffle = False
 
     elif dataset == 'surface':
 
         X_train, X_test, y_train, y_test = surface_crack_loading_and_preprocessing()
         is_time_series = False
+        shuffle = True
 
     else:
 
@@ -192,38 +203,64 @@ for idx, row in config_df.iterrows():
 
             metric = confusion_matrix
 
+
+        elif this_metric == 'Accuracy':
+
+            metric = accuracy_score
+
         else:
 
             raise Exception('Unknown metric!')
 
-        print('Computing %s for %s on %s data set!' % (this_metric, ml_algo, dataset))
+        print('Computing %s for %s on %s data set!' %
+              (this_metric, ml_algo, dataset))
+
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.8, random_state=0, shuffle=shuffle)
 
         dummy_optimizer = BaseOptimizer(hp_space=None, hpo_method=None,
                                         ml_algorithm=ml_algo,
-                                        x_train=X_train, x_test=X_test,
-                                        y_train=y_train, y_test=y_test,
+                                        x_train=X_train, x_test=X_val,
+                                        y_train=y_train, y_test=y_val,
                                         metric=metric, n_func_evals=1,
                                         random_seed=0, n_workers=1,
-                                        cross_val=False, shuffle=False,
+                                        cross_val=False, shuffle=shuffle,
                                         is_time_series=is_time_series)
 
-        best_metric_value = dummy_optimizer.train_evaluate_ml_model(params=hp_config_dict,
+        best_metric_value = dummy_optimizer.train_evaluate_ml_model(params=hp_config_dict.copy(),
                                                                     cv_mode=False, test_mode=True)
 
         dummy_trial = Trial(hp_space=None, ml_algorithm=ml_algo,
                             optimization_schedule=None, metric=metric,
                             n_runs=None, n_func_evals=None, n_workers=None,
-                            x_train=X_train, y_train=y_train, x_test=X_test,
-                            y_test=y_test, cross_val=False, shuffle=False,
+                            x_train=X_train, y_train=y_train, x_test=X_val,
+                            y_test=y_val, cross_val=False, shuffle=shuffle,
                             is_time_series=is_time_series)
 
         df_metric_value = dummy_trial.get_baseline(
             cv_mode=False, test_mode=True)
 
+        if this_metric == 'Confusion Matrix':
+
+            best_cm = ConfusionMatrixDisplay(best_metric_value).plot()
+            best_fig_name = 'confusion_%s_max_improvement.jpg' % dataset
+            plt.savefig(os.path.join(initial_path, best_fig_name))
+            plt.show()
+
+            df_cm = ConfusionMatrixDisplay(df_metric_value).plot()
+            df_fig_name = 'confusion_%s_default_values.jpg' % dataset
+            plt.savefig(os.path.join(initial_path, df_fig_name))
+            plt.show()
+
         # Assign results
+
+        if type(best_metric_value) == np.ndarray:
+
+            best_metric_value = best_metric_value.tolist()
+            df_metric_value = df_metric_value.tolist()
+
         new_metrics[dataset][this_metric][1] = best_metric_value
         new_metrics[dataset][this_metric][0] = df_metric_value
 
-with open('new_metrics.p', 'w') as pickle_file:
+with open(os.path.join(initial_path, 'new_metrics.json'), 'w') as json_file:
 
-    pickle.dump(new_metrics, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
+    json.dump(new_metrics, json_file)
